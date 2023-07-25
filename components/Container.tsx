bcb5e8767fcc0c2d098a533792534e3dbd4d9dcc
Ronald Paek <ronaldpaek@gmail.com>
2023-07-25 15:30:15 -0700

    Applied prettier rules to files

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,21 +1,21 @@
-import clsx from "clsx";
+import clsx from 'clsx'
 
 interface ContainerProps {
-  as?: React.ElementType;
-  className?: string;
-  children: React.ReactNode;
+  as?: React.ElementType
+  className?: string
+  children: React.ReactNode
 }
 
 const Container = ({
-  as: Component = "div",
+  as: Component = 'div',
   className,
   children,
 }: ContainerProps) => {
   return (
-    <Component className={clsx("mx-auto max-w-7xl px-6 lg:px-8", className)}>
+    <Component className={clsx('mx-auto max-w-7xl px-6 lg:px-8', className)}>
       <div className="mx-auto max-w-2xl lg:max-w-none">{children}</div>
     </Component>
-  );
-};
+  )
+}
 
-export default Container;
+export default Container
