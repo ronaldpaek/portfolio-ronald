45e1e607d31a29506b6d422cb48f2ada2aa1936f
Ronald Paek <ronaldpaek@gmail.com>
2023-07-25 16:05:35 -0700

    Update Button component to render as button or link

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,33 @@
-import Link from 'next/link'
 import clsx from 'clsx'
+import Link from 'next/link'
 
-const Button = () => {
-  return <div>Button</div>
+interface ButtonProps {
+  href?: string
+  className?: string
+  children: React.ReactNode
+}
+
+const Button = ({ href, className, children, ...props }: ButtonProps) => {
+  className = clsx(
+    className,
+    'inline-flex rounded-full px-4 py-1.5 text-sm font-semibold',
+  )
+
+  const inner = <span className="relative top-px">{children}</span>
+
+  if (href) {
+    return (
+      <Link href={href} className={className} {...props}>
+        {inner}
+      </Link>
+    )
+  }
+
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
