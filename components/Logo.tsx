48fd34fda69308d721c346f137e8c35568552784
Ronald Paek <ronaldpaek@gmail.com>
2023-07-26 16:45:19 -0700

    Add logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+const Logo = () => {
+  return <Image src="/rocket.svg" alt="logo" width={48} height={48} />
+}
+
+export default Logo
--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import Header from './Header'
 import Footer from './Footer'
 import Hero from './Hero'
+import Logo from './Logo'
 
-export { Header, Footer, Hero }
+export { Header, Footer, Hero, Logo }
