6579b443b0aba539f3763f0605ec90916d714e02
Ronald Paek <ronaldpaek@gmail.com>
2023-07-26 17:10:41 -0700

    Add Button component to index file

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ import Header from './Header'
 import Footer from './Footer'
 import Hero from './Hero'
 import Logo from './Logo'
+import Button from './Button'
 
-export { Header, Footer, Hero, Logo }
+export { Header, Footer, Hero, Logo, Button }
