a35acf940a6f86b2d9a29f4c24730d3601695819
Ronald Paek <ronaldpaek@gmail.com>
2023-07-27 17:17:44 -0700

    Add tailwind css spin class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
