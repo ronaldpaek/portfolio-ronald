495b98e7eca5d407fc07cc30615c89768969706c
Ronald Paek <ronaldpaek@gmail.com>
2023-07-30 16:23:45 -0700

    Update css config add some custom css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      animation: {
-        'spin-slow': 'spin 8s linear infinite',
+      colors: {
+        accent: {
+          1: 'hsl(var(--color-accent1) / <alpha-value>)',
+          2: 'hsl(var(--color-accent2) / <alpha-value>)',
+        },
+        bkg: 'hsl(var(--color-bkg) / <alpha-value>)',
+        content: 'hsl(var(--color-content) / <alpha-value>)',
       },
+    },
+    animation: {
+      'spin-slower': 'spin 35s ease infinite',
+      'spin-slow': 'spin 25s ease-in-out infinite reverse',
     },
   },
   plugins: [],
