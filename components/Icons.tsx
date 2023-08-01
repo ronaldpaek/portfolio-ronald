247c33ab91b73e68f6cd573719d354a663934e9a
Ronald Paek <ronaldpaek@gmail.com>
2023-07-31 23:58:39 -0700

    Add externallinkicon

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react'
 import { SVGProps } from 'react'
 
+export const ExternalLinkIcon = ({
+  fill,
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} {...props}>
+    <path
+      fill="currentColor"
+      d="M18 7.05a1 1 0 0 0-1-1L9 6a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L16 9.42V15a1 1 0 0 0 1 1 1 1 0 0 0 1-1Z"
+    />
+  </svg>
+)
+
 export const DownloadIcon = ({ fill, ...props }: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" {...props}>
     <path
