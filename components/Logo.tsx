7960dc418428e5b4231de08ae5f729fb6bf1de9d
Ronald Paek <ronaldpaek@gmail.com>
2023-07-31 23:58:16 -0700

    Update logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image'
 
 const Logo = () => {
   return (
-    <div className="flex items-center gap-4">
+    <div className="flex items-center gap-2 lg:gap-4">
       <div className="relative h-8 w-8">
         <Image src="/logo.svg" alt="logo" fill />
       </div>
-      <span className="hidden text-2xl font-bold lg:block">Ronald Paek.</span>
+      {/* <span className="block text-xl lg:text-2xl font-bold">Ronald Paek.</span> */}
     </div>
   )
 }
