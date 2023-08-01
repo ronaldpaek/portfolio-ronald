a7c32e46f280b9f7ee8536ea30ab37317bd56f47
Ronald Paek <ronaldpaek@gmail.com>
2023-07-31 23:55:53 -0700

    Add resume Page and resume file

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-
 const Resume = () => {
-  return <div>Resume Page</div>
+  return (
+    <iframe
+      src="/ronald-paek-standard-resume-2023.pdf"
+      className="relative top-[80px] min-h-screen w-full"
+    />
+  )
 }
 
 export default Resume
